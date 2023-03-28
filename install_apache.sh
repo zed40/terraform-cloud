509aef0468ab1d21f3ebc5e3b6934e985b8cbda5
zed40 <zedengineer10@gmail.com>
2023-03-28 04:31:36 +0000

    file2

--- install_apache.sh
+++ install_apache.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 yum update -y
+yum install -y git wget unzip
 yum install -y httpd.x86_64
 systemctl start httpd.service
 systemctl enable httpd.service
-echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+cd /var/www/html/
+wget https://github.com/kserge2001/school-site/archive/refs/heads/master.zip
+unzip master.zip
+mv school-site-master/* .
+
+
