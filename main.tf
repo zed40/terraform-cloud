32c5b1a3e6a6ae06ce2cfd81046fc7bbf0bb7ae1
zed40 <zedengineer10@gmail.com>
2023-03-28 06:26:55 +0000

    file3

--- main.tf
+++ main.tf
@@ -289,7 +289,7 @@ resource "aws_db_instance" "default" {
   allocated_storage      = 10
   db_subnet_group_name   = aws_db_subnet_group.default.id
   engine                 = "mysql"
-  engine_version         = "8.0.20"
+  engine_version         = "8.0.28"
   instance_class         = "db.t2.micro"
   multi_az               = true
   name                   = "mydb"
